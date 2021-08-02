f815b6f6ca445dc3c44d61536a73c07115250821
Maysafig <maysafig@gmail.com>
2021-08-01 21:15:14 -0300

    criação do site

---
+++ js/script.js
@@ -0,0 +1,13 @@
+document.getElementById("botaoEnviar").addEventListener("click",validaFormulario)
+
+function validaFormulario(){
+  var nome = document.getElementById("nome").value
+  var email = document.getElementById("email").value
+  var telefone = document.getElementById("telefone").value
+  
+  if(nome != "" && email != "" && telefone != ""){
+  alert("Prontinho! Você receberá as novidades por email.")  
+  }else{
+    alert("Por favor, preencha os campos nome, email e telefone.")
+  }
+}
